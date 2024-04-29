ab33a096a4b5449073f5f9f972e1d840ce9631a7
junho <jh102328@khu.ac.kr>
2024-04-29 16:29:35 +0900

    settings

---
+++ src/main/java/cc/gifthub/GifthubApplication.java
@@ -0,0 +1,13 @@
+package cc.gifthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GifthubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GifthubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cc/gifthub/GifthubApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.gifthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GifthubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
